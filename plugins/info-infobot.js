94c0004f238fb753231a0848695ca0053c65a7bd
YallicoPe <yhairmusica950@gmail.com>
2024-06-03 20:22:28 -0500

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn}) => {
   m.reply(global.infopingui);
 };
-handler.command = /^(info|infoantartida|infopingui)$/i;
+handler.command = /^(info|infobot|infopingui)$/i;
 export default handler;
 
 global.infopingui = `
-🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗜𝗻𝗳𝗼🐧
+𝗕𝗼𝘁 𝗞𝗰𝗵𝗲𝗿𝗼 💦
 
 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 : 𝗖𝗿𝗶𝘀𝘁𝗵𝗼𝗳𝗲𝗿 𝗬𝗮𝗹𝗹𝗶𝗰𝗼
 𝗡𝘂𝗺𝗲𝗿𝗼 : +51 926 933 818
