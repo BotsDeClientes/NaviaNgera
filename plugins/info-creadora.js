c4575de522f7f6bbcfac93a07501fb4d4f612f69
YallicoPe <yhairmusica950@gmail.com>
2024-06-03 20:20:55 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn}) => {
 handler.command = /^(Creador)$/i;
 export default handler;
 global.Creador = `
-𝐁𝐨𝐭 𝐀𝐧𝐭𝐚𝐫𝐭𝐢𝐝𝐚 ❄️
+𝗕𝗼𝘁 𝗞𝗰𝗵𝗲𝗿𝗼 💦
 
 𝗙𝗯 : https://web.facebook.com/profile.php?id=100094780575612
 𝗜𝗻𝘀𝘁𝗮 : https://www.instagram.com/cristhofer_yallico/
